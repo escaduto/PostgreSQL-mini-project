f79e9590ae292c78c9d75ec6ddbed8937201c154
Erica Scaduto <escaduto@ucdavis.edu>
2021-04-12 20:31:20 -0700

    added and updated new project

---
+++ src/movie_watchlist/app.py
@@ -0,0 +1,30 @@
+import database as db
+
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies
+4) Watch a movie
+5) View watched movies.
+6) Exit.
+
+Your selection: """
+welcome = "Welcome to the watchlist app!"
+
+
+print(welcome)
+db.create_tables()
+
+while (user_input := input(menu)) != "6":
+    if user_input == "1":
+        pass
+    elif user_input == "2":
+        pass
+    elif user_input == "3":
+        pass
+    elif user_input == "4":
+        pass
+    elif user_input == "5":
+        pass
+    else:
+        print("Invalid input, please try again!")
